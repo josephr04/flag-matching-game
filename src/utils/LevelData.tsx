b8d4ab3dc60ceaee90cd4b2638526c97b9025dd6
josephr04 <jrosas.dev@gmail.com>
2025-08-09 22:42:29 -0500

    feat: add data items for levels and navbar

---
+++ src/utils/LevelData.tsx
@@ -0,0 +1,19 @@
+import IMAGES from "@/assets/images/images"
+
+export const levelItems = [
+  {
+    name: "Easy",
+    description: "8 Cards",
+    img: IMAGES.easyLvl,
+  },
+  {
+    name: "Medium",
+    description: "12 Cards",
+    img: IMAGES.mediumLvl,
+  },
+  {
+    name: "Hard",
+    description: "16 Cards",
+    img: IMAGES.hardLvl,
+  },
+];
---
+++ src/utils/NavData.tsx
@@ -0,0 +1,10 @@
+export const navItems = [
+  {
+    label: "Home",
+    url: "/",
+  },
+  {
+    label: "Language",
+    url: "/",
+  },
+];
