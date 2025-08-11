a1d42143c0772f45fddd6465fe6456731d5ad604
josephr04 <jrosas.dev@gmail.com>
2025-08-10 21:47:47 -0500

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+
+export function Footer() {
+  return (
+    <div className="flex mx-auto items-center justify-center h-18 w-full bg-[#223A4E] py-4 px-4 min-w-[18em]">
+      <div className="text-white text-xs">© Copyright 2025 Joseph Rosas</div>
+    </div>
+  );
+};
