060e7eb1d9dadfa5064b8ecb35af0e52d85b9fd6
josephr04 <jrosas.dev@gmail.com>
2025-08-11 22:22:07 -0500

    feature: add context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/home';
+import Level from '@/pages/Level';
+import { CountryProvider } from './context/CountryProvider';
 import './App.css';
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CountryProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='levels/:id' element={<Level />} />
+            <Route path='*' />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CountryProvider>
   )
 }
 
