23992dbd948a1f7ff6964d5b77ef7e8cdf31e093
josephr04 <jrosas.dev@gmail.com>
2025-08-17 21:03:41 -0500

    feature: improve level page style

--- src/feature/LevelCard.tsx
+++ src/feature/LevelCard.tsx
@@ -6,11 +6,11 @@ type Card = {
 
 function CardComponent({ card }: { card: Card }) {
   return (
-    <a href={`/levels/${card.name}`} className="flex flex-col bg-white p-2 rounded-xl justify-between h-36 min-w-[6.78em]">
-      <img src={card.img} alt="Easy Level" className="h-20 rounded-xl max-w-[5.78em]"/>
+    <a href={`/levels/${card.name}`} className="flex flex-col items-center bg-white p-2 md:p-4 rounded-xl justify-between md:h-80 sm:h-50 sm:min-w-[11em] md:min-w-[18em] h-36 min-w-[7em] hover:scale-103 transition-transform duration-300 hover:shadow-lg">
+      <img src={card.img} alt="Easy Level" className="h-20 max-w-[5.78em] sm:h-31 sm:max-w-[10em] md:h-50 md:max-w-[16em] rounded-xl "/>
       <div className="flex flex-col items-center">
-        <div className="font-semibold">{card.name}</div>
-        <div className="text-sm">{card.cards} Cards</div>
+        <div className="font-semibold md:text-lg">{card.name}</div>
+        <div className="text-sm md:text-base">{card.cards} Cards</div>
       </div>
     </a>
   )
--- src/pages/Level.tsx
+++ src/pages/Level.tsx
@@ -33,47 +33,48 @@ export default function Level() {
   };
 
   return (
-    <div className="font-[Roboto]">
-      <div className="relative bg-[url(/src/assets/images/bg-flags.jpeg)] bg-cover min-h-[2em] min-w-[18em]">
+    <div className="font-[Roboto] min-w-[18em] min-h-screen flex flex-col">
+      {/* Header section */}
+      <div className="relative bg-[url(/src/assets/images/bg-flags.jpg)] bg-cover min-h-[9em] md:min-h-[13em] min-w-[18em] flex-shrink-0">
         <div className="absolute inset-0 bg-[#223A4E] opacity-75"></div>
-        <div className="overlay w-full h-12 z-5"></div>
-      </div>
+        <div className="absolute overlay inset-0 w-full h-full z-5"></div>
 
-      <div className="flex flex-col bg-[#223A4E] min-h-[6em] w-full">
-        <div className="w-fit pt-5 pb-2">
-          <div className="text-xl font-bold text-white pl-5">
-            Level {id}
+        <div className="absolute z-5 flex flex-col justify-end h-full pb-4">
+          <div className="w-fit pb-2">
+            <div className="text-xl md:text-3xl font-bold text-white pl-5 md:pl-8">
+              Level {id}
+            </div>
+            <div className="bg-[#14A5BA] h-1 rounded-r-2xl"></div>
           </div>
-          <div className="bg-[#14A5BA] h-1 rounded-r-2xl"></div>
-        </div>
-        <div className="pl-5 text-white text-xs w-60">
-          Here you have {cardsNumber} cards. Find and match all the country flags!
+          <div className="text-white text-xs md:text-base w-60 md:w-md pl-5 md:pl-8">
+            Here you have {cardsNumber} cards. Find and match all the country flags!
+          </div>
         </div>
       </div>
 
-      <div className="flex justify-center items-center bg-[#223A4E] py-2 min-h-[2.5em]">
+      {/* Timer/Start button section */}
+      <div className="flex justify-center items-center bg-[#223A4E] py-2 md:py-5 min-h-[2.5em] flex-shrink-0">
         {!gameStarted ? (
-          <button onClick={handleStartGame} className="bg-[#14A5BA] px-8 py-2 rounded-lg text-white hover:bg-[#0d8fa3] transition cursor-pointer">
+          <button onClick={handleStartGame} className="bg-[#14A5BA] px-8 py-2 md:text-lg md:px-8 md:py-2 rounded-lg text-white hover:bg-[#0d8fa3] transition cursor-pointer">
             Start
           </button>
         ) : !gameWon ? (
-          <div className="text-white font-medium text-sm">
+          <div className="text-white font-medium text-sm md:text-lg">
             Time: <span>{formatTime(time)}</span>
           </div>
         ) : null}
       </div>
 
-      <div className="bg-[#223A4E] min-h-[26em]">
-        <div>
-          <GameCardList 
-            key={resetKey} 
-            cardsNumber={cardsNumber ?? 0} 
-            gameStarted={gameStarted}
-            onTimeUpdate={setTime}
-            onGameWon={handleGameWon}
-            onRestart={handleRestartGame}
-          />
-        </div>
+      {/* Cards section */}
+      <div className="bg-[#223A4E] flex-grow overflow-auto">
+        <GameCardList 
+          key={resetKey} 
+          cardsNumber={cardsNumber ?? 0} 
+          gameStarted={gameStarted}
+          onTimeUpdate={setTime}
+          onGameWon={handleGameWon}
+          onRestart={handleRestartGame}
+        />
       </div>
     </div>
   )
