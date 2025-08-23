f5034e34501ef9c20455848b218ddd2293ae44f7
josephr04 <jrosas.dev@gmail.com>
2025-08-22 20:07:49 -0500

    feature: add vercel analytics

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 function Layout() {
   return (
     <div className="w-full">
       <Header />
       <Outlet />
+      <Analytics />
       <Footer />
     </div>
   );
